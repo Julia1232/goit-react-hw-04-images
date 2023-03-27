658292e44f10610c72db40cab2c313826aea5289
Julia Petrenko <juliapetrenko02.07.1999@gmai.com>
2023-03-27 17:05:50 +0300

    eleven

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,36 +1,28 @@
-import React from 'react'
-import ImageGalleryItem from 'components/ImageGalerryItem/ImageGalerryItem'
-import style from '../ImageGallery/ImageGallery.module.css'
-import PropTypes from 'prop-types'
+import React from 'react';
+import ImageGalleryItem from 'components/ImageGalerryItem/ImageGalerryItem';
+import style from '../ImageGallery/ImageGallery.module.css';
+import PropTypes from 'prop-types';
 
+const ImageGallery = ({ images, onImageClick }) => (
+  <ul className={style.ImageGallery}>
+    {images.map(image => {
+      return (
+        <ImageGalleryItem
+          key={image.id}
+          image={image}
+          onImageClick={onImageClick}
+        />
+      );
+    })}
+  </ul>
+);
 
-const ImageGallery = ({ images, onImagesClick }) => (
-
-    <ul className={style.ImageGallery}>
-        {images.map(image=> {
-            return (
-    
-        <ImageGalleryItem 
-        key={image.id}
-        image={image}
-        onImagesClick={onImagesClick} />
-            );
-            
-            
-        })}
-
-     </ul>
-)
-
-  
 ImageGallery.propTypes = {
   images: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
-    }),
+    })
   ),
 };
 
-
-
-export default ImageGallery
+export default ImageGallery;
