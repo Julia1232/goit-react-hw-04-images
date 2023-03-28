792d01755be8dfe71c36bb7523d907fc120d9db9
Julia Petrenko <juliapetrenko02.07.1999@gmai.com>
2023-03-28 16:08:45 +0300

    fifth

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,43 +1,41 @@
 import React, { Component } from 'react';
- import { createPortal } from 'react-dom';
- import PropTypes from 'prop-types';
- import styles from '../Modal/Modal.module.css';
- 
+import { createPortal } from 'react-dom';
+import PropTypes from 'prop-types';
+import styles from '../Modal/Modal.module.css';
 
-const modalRoot = document.querySelector('#modal-root');
- 
+const modalRoot = document.querySelector('#root');
 
- class Modal extends Component {
-   componentDidMount() {
-     window.addEventListener('keydown', this.handleKeyDown);
-   }
-   componentWillUnmount() {
-     window.removeEventListener('keydown', this.handleKeyDown);
-   }
-   handleKeyDown = event => {
-     if (event.code === 'Escape') {
-       this.props.onClose();
-     }
-   };
-   handleBackdropClick = event => {
-     if (event.currentTarget === event.target) {
-       this.props.onClose();
-     }
-   };
-   render() {
-     return createPortal(
-       <div className={styles.Overlay} onClick={this.handleBackdropClick}>
-         <div className={styles.Modal}>{this.props.children}</div>
-       </div>,
-       modalRoot,
-     );
-   }
- }
- Modal.defaultProps = {
-   children: null,
- };
- Modal.propTypes = {
-   children: PropTypes.node,
-   onClose: PropTypes.func.isRequired,
- };
- export default Modal;
+class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+  handleKeyDown = event => {
+    if (event.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+  handleBackdropClick = event => {
+    if (event.currentTarget === event.target) {
+      this.props.onClose();
+    }
+  };
+  render() {
+    return createPortal(
+      <div className={styles.Overlay} onClick={this.handleBackdropClick}>
+        <div className={styles.Modal}>{this.props.children}</div>
+      </div>,
+      modalRoot
+    );
+  }
+}
+Modal.defaultProps = {
+  children: null,
+};
+Modal.propTypes = {
+  children: PropTypes.node,
+  onClose: PropTypes.func.isRequired,
+};
+export default Modal;
